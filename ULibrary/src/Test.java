b93ddb43f499e4e263c15c1edb96a6298f6d5b7e
tofu69 <kobebry48@gmail.com>
2013-03-23 15:31:04 -0700

    Revert "yolo test#2"
    This reverts commit e9febc5324cbb0249d2c9b6b0c5467242d06485d.

--- ULibrary/src/Test.java
+++ ULibrary/src/Test.java
@@ -3,6 +3,5 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Testing test.");
 		System.out.println("#YOLOSWAG");
-		System.out.println("testing push YOLO");
 	}
 }
