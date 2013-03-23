e9febc5324cbb0249d2c9b6b0c5467242d06485d
tofu69 <kobebry48@gmail.com>
2013-03-23 15:26:56 -0700

    yolo test#2
    yolo

--- ULibrary/src/Test.java
+++ ULibrary/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Testing test.");
 		System.out.println("#YOLOSWAG");
+		System.out.println("testing push YOLO");
 	}
 }
