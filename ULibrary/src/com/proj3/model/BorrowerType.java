cba79a485d8ddce0c21be61ff729fca598549aa2
Red <kobebry48@gmail.com>
2013-03-26 17:00:48 -0700

    Defined BorrowerTypes based on table

--- ULibrary/src/com/proj3/model/BorrowerType.java
+++ ULibrary/src/com/proj3/model/BorrowerType.java
@@ -1,7 +1,17 @@
 package com.proj3.model;
 
 public enum BorrowerType {
-	faculty,
-	student,
-	staff
+	faculty ("fal"),
+	student ("stu"),
+	staff ("sta");
+	
+	private String type;
+	
+	private BorrowerType(String type){
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
 }
