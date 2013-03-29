5415a430913e3fd3a1b646ad569ac57d5bef5fc9
takdeerkaur <takdeerkaur@gmail.com>
2013-03-29 15:40:23 -0700

    fine

--- fake_data_fine.sql
+++ fake_data_fine.sql
@@ -1,63 +1,63 @@
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'12.34',
+	12.34,
 	'2013-03-28 03:14:07',
 	'2013-03-30 03:14:07',
-	'3'
+	3
 );
 
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'5.64',
+	5.64,
 	'2013-03-21 02:18:07',
 	'NULL',
-	'5'
+	5
 );
 
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'89.21',
+	89.21,
 	'2013-02-23 05:14:20',
 	'NULL',
-	'7'
+	7
 );
 
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'13.21',
+	13.21,
 	'2013-03-28 03:14:08',
 	'NULL',
-	'9'
+	9
 );
 
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'12.34',
+	12.34,
 	'2013-02-25 03:14:07',
 	'2013-03-30 03:14:00',
-	'9'
+	9
 );
 
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'5.64',
+	5.64,
 	'2013-03-21 02:18:07',
 	'NULL',
-	'7'
+	7
 );
 
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'89.21',
+	89.21,
 	'2013-02-23 05:14:20',
 	'NULL',
-	'5'
+	5
 );
 
 INSERT INTO Fine VALUES(
 	fid_counter.nextVal,
-	'13.21',
+	13.21,
 	'2013-03-28 03:14:08',
 	'NULL',
-	'3'
+	3
 );
