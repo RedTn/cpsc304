02f34b271f56a37454ed48d3e4140b6e062129fa
Jae-Hwan (Jeff) Jung <midlife83@gmail.com>
2013-03-28 18:54:36 -0700

    added borrowing.sql

---
+++ fake_data_borrowing.sql
@@ -0,0 +1,17 @@
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 3, 'ROWLING HPPhilosopher', 1, '2017-10-10', '2017-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 3, 'ROWLING HPPhilosopher', 3, '2017-8-10', '2017-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 3, 'ROWLING HPPhilosopher', 5, '2017-7-10', '2017-7-12');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 3, 'ROWLING HPPhilosopher', 6, '2017-10-10', '2017-10-13');
+
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 5, 'ROWLING HPChamber', 1, '2017-10-10', '2017-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 5, 'ROWLING HPChamber', 4, '2013-3-3', '2013-3-25');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 5, 'ROWLING HPChamber', 5, '2017-1-10', '2017-1-12');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 5, 'ROWLING HPChamber', 6, '2017-1-10', '2017-1-27');
+
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 7, 'ROWLING HPPrisoner', 1, '2017-10-10', '2017-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 9, 'ROWLING HPGoblet', 1, '2017-12-10', '2017-12-13');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 11, 'ROWLING HPOrder', 1, '2015-10-10', '2017-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 3, 'ROWLING HPHalf', 1, '2014-10-10', '2017-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 5, 'ROWLING HPDeathly', 1, '2013-7-10', '2017-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 7, 'STEWART Calculus', 3, '2012-8-10', '2014-10-10');
+INSERT INTO Borrowing VALUES(borid_counter.nextVal, 9, 'DAWKINS Selfish', 10, '2011-10-10', '2011-11-10');
--- fake_data_holdrequest.sql
+++ fake_data_holdrequest.sql
@@ -1,25 +1,25 @@
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747532699', '2015-10-10');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747532699', '2015-11-11');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747532699', '2015-12-12');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, 'ROWLING HPPhilosopher', '2015-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, 'ROWLING HPPhilosopher', '2015-11-11');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, 'ROWLING HPPhilosopher', '2015-12-12');
 
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747538492', '2012-10-10');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747538492', '2013-10-10');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0747538492', '2014-10-10');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0747538492', '2015-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, 'ROWLING HPChamber', '2012-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 7, 'ROWLING HPChamber', '2013-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 9, 'ROWLING HPChamber', '2014-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 9, 'ROWLING HPChamber', '2015-10-10');
 
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747542155', '2017-10-10');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '074754624X', '2018-10-10');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0747551006', '2019-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, 'ROWLING HPPrisoner', '2017-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, 'ROWLING HPGoblet', '2018-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 7, 'ROWLING HPOrder', '2019-10-10');
 
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-1-5');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-2-10');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-3-15');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-4-20');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 9, 'ROWLING HPHalf', '2013-1-5');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 9, 'ROWLING HPHalf', '2013-2-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 9, 'ROWLING HPHalf', '2013-3-15');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 9, 'ROWLING HPHalf', '2013-4-20');
 
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0534251609', '2011-2-2');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0534251609', '2011-5-5');
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0534251609', '2011-7-7');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, 'ROWLING HPDeathly', '2011-2-2');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, 'ROWLING HPDeathly', '2011-5-5');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 9, 'ROWLING HPDeathly', '2011-7-7');
 
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0310941784', '2020-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 7, 'STEWART Calculus', '2020-10-10');
 
-INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0199291152', '2020-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 7, 'BIBLE', '2020-10-10');
