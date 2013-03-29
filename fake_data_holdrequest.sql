7088193db6c1ab670497ca001e7339fe2a50a1ee
Jae-Hwan (Jeff) Jung <midlife83@gmail.com>
2013-03-28 18:22:09 -0700

    added bookcopy and holdrequest

---
+++ fake_data_holdrequest.sql
@@ -0,0 +1,25 @@
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747532699', '2015-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747532699', '2015-11-11');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747532699', '2015-12-12');
+
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747538492', '2012-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747538492', '2013-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0747538492', '2014-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0747538492', '2015-10-10');
+
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0747542155', '2017-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '074754624X', '2018-10-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0747551006', '2019-10-10');
+
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-1-5');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-2-10');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-3-15');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0747581088', '2013-4-20');
+
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 1, '0534251609', '2011-2-2');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0534251609', '2011-5-5');
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0534251609', '2011-7-7');
+
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 3, '0310941784', '2020-10-10');
+
+INSERT INTO HoldRequest VALUES(hid_counter.nextVal, 5, '0199291152', '2020-10-10');
